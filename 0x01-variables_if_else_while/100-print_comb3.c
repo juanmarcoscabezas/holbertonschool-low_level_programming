f1e253f6482e12de43964433205b002d3e4248e3
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-09-19 19:07:10 +0000

    Working on advanced task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function print the alphabet a...z
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int aux;
+	int aux2;
+
+	for (aux = 0; aux < 10; aux++)
+	{
+		for (aux2 = 0; aux2 < 10; aux2++)
+		{
+			
+			if (aux != aux2 && aux < aux2)
+			{
+				putchar((aux % 10) + '0');
+				putchar((aux2 % 10) + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
