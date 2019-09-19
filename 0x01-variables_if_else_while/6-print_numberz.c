ff48620df09b85849fd2bd30f205c4b724adec07
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-09-19 18:09:08 +0000

    Working on task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function print numbers from 0...9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int aux;
+
+	for (aux = 0; aux < 10; aux++)
+	{
+		putchar((aux % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
