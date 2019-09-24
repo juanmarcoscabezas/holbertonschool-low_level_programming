6b94d9e3098f21e53b01e4acc135545720c606ec
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-09-24 18:01:29 +0000

    Working on advanced task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the 50 fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int start;
+	long int fibonacci = 1;
+	long int last = 1;
+
+	for (start = 0; start < 50; start++)
+	{
+		if (start < 49)
+			printf("%ld, ", fibonacci);
+		else
+			printf("%ld", fibonacci);
+		long int aux = fibonacci;
+		fibonacci = fibonacci + last;
+		last = aux;	
+	}
+	putchar('\n');
+	return (0);
+}
