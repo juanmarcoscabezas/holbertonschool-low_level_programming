6e261de317b2f50f170abda6e2e4816084a5cb2a
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-09-24 18:50:32 +0000

    Working on task 0

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -4,6 +4,13 @@
 #include <stdio.h>
 
 /**
+ * positive_or_negative - return the sign of a number
+ * @i: number
+ * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
+ */
+void positive_or_negative(int i);
+
+/**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
