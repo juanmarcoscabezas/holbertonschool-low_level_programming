cd977f7526298bf36d4c11e7d20d7cea0b688c87
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-09-24 14:52:51 +0000

    Fixing task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,59 +1,46 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - Entry point
- *
  * Description: This function prints the numbers from n to 98
- *
  * @n: Start number of print
- *
- * Return: 
+ * Return:
  */
 void print_to_98(int n)
 {
-	
-	int start;
-	int n_div = 1;
-	int n_copy = n;
+	int start = n;
+	int number;
 
-	while(n_copy > 9)
+	while (start != 98)
 	{
-		n_copy = n_copy / 10;
-		n_div = n_div * 10;
-	}
-	if (n > 98)
-	{
-		for (start = n; start >= 98; start--)
+		int aux = start;
+
+		if (start < 0)
+			_putchar('-');
+		for (number = 100; number >= 10; number /= 10)
 		{
-			int start_copy = start;
-			int n_div_copy = n_div;
-			int n_div_aux = n_div;
-			while (start_copy > 9)
+			if (aux / number > 0)
 			{
-				start_copy = start_copy / 10;
-				n_div_copy = n_div_copy / 10;
-				if (start_copy <= 9)
-				{
-					_putchar(start_copy + '0');
-					n_div_aux = n_div_aux / 10;
-					n_div_copy = n_div_aux;
-					start_copy = start_copy / n_div_copy;
-				}
+				_putchar((aux / number) + '0');
+				aux = aux % number;
 			}
-			_putchar((start % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (aux / number < 0)
+			{
+				_putchar(((start / number) * -1) + '0');
+			}
 		}
+		if (start >= 0)
+			_putchar((start % 10) + '0');
+		else
+			_putchar(((start % 10) * -1) + '0');
+		_putchar(',');
+		_putchar(' ');
+		if (n > 98)
+			start--;
+		else
+			start++;
 	}
-	else if (n == 98)
-	{
-		_putchar('9');
-		_putchar('8');
-	}
-	else
-	{
-		
-	}
+	_putchar('9');
+	_putchar('8');
 	_putchar('\n');
 }
