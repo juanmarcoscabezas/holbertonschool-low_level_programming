c2e3bd96b06aa2a6f0311a11c2fb3761ef2197a7
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-09-24 17:11:54 +0000

    Working on advance task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,16 +2,15 @@
 
 /**
  * print_times_table - Entry point
- *
  * Description: This function prints the tables from 0 to 15
- *
+ * @n: the number that i wanna show
  * Return:
  */
 void print_times_table(int n)
 {
 	int number;
 	int mult;
-	
+
 	if (n < 15 && n >= 0)
 	{
 		for (number = 0; number <= n; number++)
@@ -20,23 +19,26 @@ void print_times_table(int n)
 			{
 				int res = number * mult;
 
+				if (mult > 0)
+					_putchar(',');
+				if (mult > 0 && res < 1000)
+					_putchar(' ');
+				if (mult > 0 && res < 100)
+					_putchar(' ');
+				if (mult > 0 && res < 10)
+					_putchar(' ');
 				if (res > 99)
 				{
 					_putchar((res / 100) + '0');
 					res = res % 100;
+					if (res < 10)
+						_putchar('0');
 				}
 				if ((res > 9) && (res < 100))
 				{
 					_putchar((res / 10) + '0');
 				}
 				_putchar((res % 10) + '0');
-				if (mult < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar
-				}
 			}
 			_putchar('\n');
 		}
