13bec591f30990d9310cf58299155fe1a66a37a3
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-09-24 09:02:47 +0000

    Fixing erros on task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
+
 /**
- * main - Entry point
- * 
- * Description: This function prints the alphabet x10 times using the function _putchar
- * 
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: This function prints the alphabet x10
+ * times using the function _putchar
+ *
  * Return: Always 0
  */
-
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	char letter;
 	int times;
 
 	for (times = 0; times < 10; times++)
-	{	
+	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
 		_putchar('\n');
 	}
-	return;
 }
