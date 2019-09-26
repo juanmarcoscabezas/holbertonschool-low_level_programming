6cda88b752dd7bb834bbfe648d5b19a9a4010edd
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-09-26 20:10:25 +0000

    Fixing task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,38 +9,30 @@ int main(void)
 {
 	long int number = 612852475143;
 	long int last_prime;
-	long int start;
+	long int start = 2;
 	long int is_prime = 2;
 	int valid_prime;
 
 	while (number > 1)
 	{
 		valid_prime = 1;
-
-		for (start = 1; start <= is_prime; start++)
+		if (number % start == 0)
 		{
-			if (is_prime % start == 0 && start != 1 && start != is_prime)
+			for (is_prime = 1; is_prime <= start; is_prime++)
 			{
-				valid_prime = 0;
+				if (start % is_prime == 0 && is_prime != 1 && is_prime != start)
+				{
+					valid_prime = 0;
+				}
+			}
+			if ( valid_prime == 1)
+			{
+				number = number / start;
+				last_prime = start;
+				start = 1;
 			}
 		}
-		if ((valid_prime == 1) && (number % is_prime == 0))
-		{
-			last_prime = is_prime;
-			number = number / is_prime;
-			/* printf("\nGOTCHA %ld, now IM %ld \n", is_prime, number);*/
-			is_prime = 2;
-		}
-		else
-		{
-			is_prime++;
-		}
-
-		if (number <= 1)
-		{
-			break;
-		}
-		printf("%ld, ", is_prime);
+		start ++;
 	}
 	printf("%ld\n", last_prime);
 	return (0);
