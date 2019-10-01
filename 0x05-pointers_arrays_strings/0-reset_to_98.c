7e66f50676a32ccc84717ab0822b3875a5f2d9d9
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-01 14:14:08 +0000

    Working on task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - Reset pointer to 98
+ * Description: This function reset a pointer to the value 98
+ * @n: Pointer that gonna change value
+ * Return:
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * _putchar - putchar function
+ *
+ * Description: This function prints characters
+ *
+ * @c: is the entry character that has to be printed
+ *
+ * Return: void
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - Reset pointer to 98
+ * Description: This function reset a pointer to the value 98
+ * @n: Pointer that gonna change value
+ * Return:
+ */
+void reset_to_98(int *n);
+
+
+
+#endif
