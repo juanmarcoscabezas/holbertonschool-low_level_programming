00da2a287ac3877e4b05cd2a5f3ad6840e98cd70
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-01 19:12:18 +0000

    Working on task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * _strcpy - Copy a string
@@ -11,6 +10,7 @@
 char *_strcpy(char *dest, char *src)
 {
 	int start = 0;
+
 	while (*src != '\0')
 	{
 		*dest = *src;
