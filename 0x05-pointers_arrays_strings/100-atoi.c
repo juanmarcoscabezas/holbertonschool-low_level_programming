d19582f774cfefc1af1344a452f1fb8b0a9f41bf
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-02 03:21:52 +0000

    Fixing advanced task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,8 +9,7 @@
  */
 int _atoi(char *s)
 {
-	int start = 0, number = 0, number_sign = 1, number_copy = 0, ten_pow = 1;
-	int ten_pow_rev = 1; /*Variable to revert the number*/
+	int start = 0, number = 0, number_sign = 1;
 	char digit; /*Variable to compare number characteres*/
 	int digit_int; /*Variable to mult by pow*/
 	int found_int = 0; /*variable that knows if the 1st int was found*/
@@ -28,8 +27,7 @@ int _atoi(char *s)
 			{
 				found_int = 1;
 				s_is_digit = 1;
-				number = number + (ten_pow * digit_int);
-				ten_pow = ten_pow * 10;
+				number = (number * 10 + digit_int);
 				break;
 			}
 			digit_int++;
@@ -37,15 +35,6 @@ int _atoi(char *s)
 		if (found_int == 1 && s_is_digit == 0)
 			break;
 		start++;
-	}
-	number_copy = number;
-	number = 0;
-	ten_pow = ten_pow / 10;
-	while (ten_pow >= 1)
-	{
-		number = number + ((number_copy / ten_pow) % 10) * ten_pow_rev;
-		ten_pow = ten_pow / 10;
-		ten_pow_rev = ten_pow_rev * 10;
 	}
 	return (number * number_sign);
 }
