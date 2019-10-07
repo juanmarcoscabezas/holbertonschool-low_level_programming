c5e7cd3f76f748917c0b86e01e21af55bf0e2cd5
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-07 20:32:55 +0000

    Working on task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,18 +6,23 @@
  * in @haystack
  * @haystack: Pointer that contains the string
  * @needle: Pointer that contains the substring to find in @haystack
- * Return: A pointer to the beginning of the located substring 
+ * Return: A pointer to the beginning of the located substring
  * in @accept, or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int next;
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-	for (; *(s + i) != '\0', *(needle + j) != '\0'; i++)
+	while (*(haystack + i) != '\0')
 	{
-		if
+		while (*(needle + j) != '\0' && *(haystack + i) == *(needle + j))
+		{
+			j++;
+		}
+		if (j > 0)
+			return (haystack + i);
+		i++;
 	}
-	return (s);
+	return ("\0");
 }
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -63,6 +63,16 @@ unsigned int _strspn(char *s, char *accept);
  */
 char *_strpbrk(char *s, char *accept);
 
+/**
+ * _strstr - Locates a substring
+ * Description: This function locates a substring @needle
+ * in @haystack
+ * @haystack: Pointer that contains the string
+ * @needle: Pointer that contains the substring to find in @haystack
+ * Return: A pointer to the beginning of the located substring
+ * in @accept, or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle);
 
 
 
