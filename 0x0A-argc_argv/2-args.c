0cefd86580459aabeaf1612c294f64bc7b6cd531
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-13 00:47:04 +0000

    Working on task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,15 +3,19 @@
 
 /**
  * main - Entry point
- * Description: This function prints the
- * number of arguments passed into it
+ * Description: This function pritns all
+ * arguments it receives
  * @argc: Number of params in argv
  * @argv: Parameters of the function
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
-	argv++;
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
