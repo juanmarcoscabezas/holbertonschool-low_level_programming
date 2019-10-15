1166362aba19c761c4076edf8985b678e05588eb
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-15 20:47:36 +0000

    Free memory, decrement i

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			for (; i >= 0; i--)
+			for (i--; i >= 0; i--)
 			{
 				free(grid[i]);
 			}
