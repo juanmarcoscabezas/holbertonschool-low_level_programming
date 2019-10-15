e63da3b553e1092d03427401e65ed1b61f18ece7
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-15 18:41:44 +0000

    Fixing task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,16 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * _concat - concatenate strings
+ * Description: This function concatenates two strings
+ * @s: The string to concatenates
+ * @original: The original string
+ * @start: The start of the copy
+ * @end: The end of copy
+ * Return:
+ */
 void _concat(char *s, char *original, int start, int end)
 {
 	int i, k;
 
-	for (i = start, k= 0; i < end; i++, k++)
+	for (i = start, k = 0; i < end; i++, k++)
 	{
 		*(s + i) = *(original + k);
 	}
 }
 
+/**
+ * string_length - Knows the length of a string
+ * Descriotion: THis function knows the length of a
+ * given string
+ * @s: The given string
+ * Return: This function return the length of @s
+ */
 int string_length(char *s)
 {
 	int i = 0;
@@ -26,6 +42,7 @@ int string_length(char *s)
 	}
 	return (i);
 }
+
 /**
  * str_concat - Concatenates two strings
  * Description: This function concatenates two strings
@@ -43,8 +60,12 @@ char *str_concat(char *s1, char *s2)
 	size1 = string_length(s1);
 	size2 = string_length(s2);
 
-	p = malloc(sizeof(char) * (size1 + size2));
+	p = malloc(sizeof(char) * (size1 + size2) + 1);
 
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 	_concat(p, s1, 0, size1);
 	_concat(p, s2, size1, size1 + size2);
 
