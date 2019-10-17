0a03a2882c96c184198213a54dabb48d379d9b1f
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-17 21:26:46 +0000

    Working on advanced tasl 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -43,6 +43,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	a = malloc(new_size);
+	if (a == NULL)
+		free(ptr);
+		return (NULL);
 	if (new_size > old_size)
 		_memcpy(a, ptr, old_size);
 	else
