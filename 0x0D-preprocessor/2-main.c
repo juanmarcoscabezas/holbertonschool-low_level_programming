555144c6dd73f30cdcd6e56e30cf45af870099a1
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-21 18:03:49 +0000

    Workin task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -7,10 +7,9 @@
  * @argv: Value of arguments
  * Return: Always 0
  */
-int main(int argc, char* argv[])
+int main(void)
 {
-	argc++;
-	printf("%s\n", argv[0]);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
