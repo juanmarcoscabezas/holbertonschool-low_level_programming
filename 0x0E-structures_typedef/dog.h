6b058e1cafe18add526cefa2cefafe60ff61f8bb
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-21 18:24:32 +0000

    Working on task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef STRUCTFILE
+#define STRUCTFILE
+
+/**
+ * struct dog - Dog´s structure
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
