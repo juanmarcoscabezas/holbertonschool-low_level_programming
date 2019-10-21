bacbff22eb97dbcdb64e17530a7d01170fe7a539
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-21 18:04:40 +0000

    Workin task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,8 +3,6 @@
 /**
  * main - Entry point
  * Description: This function prints the name of the file
- * @argc: Number of arguments
- * @argv: Value of arguments
  * Return: Always 0
  */
 int main(void)
