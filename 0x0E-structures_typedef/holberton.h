f6867dd9b57ed3fc6169aecf6de2f8dcfb174ee6
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-21 16:55:51 +0000

    First commit

---
+++ 0x0E-structures_typedef/holberton.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * _putchar - putchar function
+ *
+ * Description: This function prints characters
+ *
+ * @c: is the entry character that has to be printed
+ *
+ * Return: void
+ */
+int _putchar(char c);
+
+
+
+
+
+
+
+#endif
