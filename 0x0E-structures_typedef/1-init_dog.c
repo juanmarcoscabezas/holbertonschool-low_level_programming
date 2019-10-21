44c5b8b30ce699d7c9008dad6abd2c9e9db4d721
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-21 18:32:14 +0000

    Working on tas 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializatio of dog´s
+ * Description: This function initializes a dog
+ * @d: Dog´s structure
+ * @name: Dog´s name
+ * @age: Dog´s age
+ * @owner: DOg´s owner
+ * Return:
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,18 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * init_dog - Initializatio of dog´s
+ * Description: This function initializes a dog
+ * @d: Dog´s structure
+ * @name: Dog´s name
+ * @age: Dog´s age
+ * @owner: DOg´s owner
+ * Return:
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
+
 #endif
