29e625989676782af49033ae7fcc15fc95888fa8
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-21 18:43:58 +0000

    Adding null possibilitie to params

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,16 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n", d->name);
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (null)\n");
+
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			pritf("Owner: (null)\n");
 	}
 }
