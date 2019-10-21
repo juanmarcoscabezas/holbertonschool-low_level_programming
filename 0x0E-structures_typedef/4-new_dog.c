018723614b77e8341fba57610144104bce24c092
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-21 19:44:53 +0000

    Changing while

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * new_dog - Creates a dog
@@ -13,7 +12,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *my_dog;
 	char *name_cpy, *owner_cpy;
-	int i = 0, j = 0;
+	int i = 0, j = 0, k;
 
 	my_dog = malloc(sizeof(struct dog));
 	if (my_dog == NULL)
@@ -35,18 +34,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(my_dog);
 		return (NULL);
 	}
-	while (*(name + i) != '\0')
+	for (k = 0; k <= i; k++)
 	{
-		*(name_cpy + i) = *(name + i);
-		i++;
+		*(name_cpy + k) = *(name + k);
 	}
-	*(name_cpy + i) = *(name + i);
-	while (*(owner + j) != '\0')
+	for (k = 0; k <= j; k++)
 	{
-		*(owner_cpy + j) = *(owner + j);
-		j++;
+		*(owner_cpy + k) = *(owner + k);
 	}
-	*(owner_cpy + j) = *(owner + j);
 	my_dog->name = name_cpy;
 	my_dog->age = age;
 	my_dog->owner = owner_cpy;
