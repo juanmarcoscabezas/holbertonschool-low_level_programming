a167cf95c95eff0d22404ad77ee884ac68b5efed
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-23 17:20:21 +0000

    NULL exceptions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,8 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
+	if (array && action && size > 0)
 	{
-		action(*(array + i));
+		for (i = 0; i < size; i++)
+		{
+			action(*(array + i));
+		}
 	}
 }
