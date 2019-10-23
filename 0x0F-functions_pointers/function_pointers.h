c4d672129f902f51f5b4454d6bd9297f11ca453a
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-23 15:38:34 +0000

    Working on task 2

--- 0x0F-functions_pointers/function_pointers.h
+++ 0x0F-functions_pointers/function_pointers.h
@@ -30,6 +30,16 @@ void print_name(char *name, void (*f)(char *));
  */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/**
+ * int_index - Searches for an integer
+ * Description: This function searches for an integer
+ * @array: Array to execute the given function
+ * @size: Size of @array
+ * @cmp: The given function to compare integers from @array
+ * Return: Returns the index of the 1st element which the cmp
+ * function does not return 0, if no element marches, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int));
 
 
 
