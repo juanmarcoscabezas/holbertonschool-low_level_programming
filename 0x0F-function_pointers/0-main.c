7b62dabc7b1920d3339c24d724853549bf6be73f
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-23 17:11:33 +0000

    Adding NULL verification

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -47,6 +47,7 @@ int main(void)
 {
     print_name("Danton", print_name_as_is);
     print_name("Rick Harris", print_name_uppercase);
+    print_name(NULL, print_name_uppercase);
     printf("\n");
     return (0);
 }
