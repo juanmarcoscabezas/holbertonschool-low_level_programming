fa1be58c9673d7e6d1f42dbe6d6ddeec6a3a903b
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-23 23:08:54 +0000

    Working on advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry function
+ * Description: This function prints the opcodes of its own main function
+ * @argc: Number of parameters
+ * @argv: Array of parameters
+ * Return: 1 if @argc != 2, 2 if atoi of @argv[1] is negative, 0 otherwise
+ */
 int main(int argc, char *argv[])
 {
-	int (*m)(int, char *) = main;
 	int i, value;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 	value = atoi(argv[1]);
+	if (value < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
 	for (i = 0; i < value; i++)
 	{
-		/*printf("%d ", *(m + i));*/
+		printf("%2hhx", *((char *) main + i));
+		if (i < value - 1)
+			printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
