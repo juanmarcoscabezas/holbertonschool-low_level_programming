7464706b271318e035cbb481cc9baa02678b2e04
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-26 23:00:49 +0000

    This should work

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,31 +1,14 @@
-	.file	"100-hello.c"
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton\n"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movq	$.LC0, -8(%rbp)
-	movq	-8(%rbp), %rax
-	movl	$18, %edx
-	movq	%rax, %rsi
-	movl	$1, %edi
-	call	write
-	movl	$0, %eax
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 5.5.0-12ubuntu1~14.04) 5.5.0 20171010"
-	.section	.note.GNU-stack,"",@progbits
+	global	main
+
+	section	.message
+main:	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, holberton
+	mov	rdx, 17
+	syscall
+	mov	rax, 60
+	xor	rdi, rdi
+	syscall
+
+	section	.data
+holberton:db	"Hello, Holberton", 10
