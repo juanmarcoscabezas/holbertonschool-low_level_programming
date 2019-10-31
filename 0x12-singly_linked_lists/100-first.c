5481bbd1ff81d8f3ea0a6e6769907aacaa21682c
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-31 23:12:04 +0000

    Advanced task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * init - Inits before main
+ * Description: This function starts before main
+ * @void: Void param
+ * Return: Always 0;
+ */ 
+static int init(void)
+{
+	printf("Hola\n");
+	return (0);
+}
