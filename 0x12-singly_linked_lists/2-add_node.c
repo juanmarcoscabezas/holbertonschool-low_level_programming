6f497d726620afc7901c5696af1f352166040a56
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-31 18:57:14 +0000

    Adding task 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,17 +5,28 @@
  * Description: Function that adds a new node
  * in a linked list
  * @head: Head of the list
- * @str: Name to add to the node 
+ * @str: Name to add to the node
  * Return: A size_t of @h
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
+	char *s;
 
 	if (!new)
 		return (NULL);
-	new->next = (list_t *) head;
-	new->str = strdup(str);
+	s = strdup(str);
+	if (!s)
+		return (NULL);
+	new->str = s;
 	new->len = strlen(str);
-	return (&new);
+	if (!head)
+		*head = new;
+	else
+	{
+		new->next = *head;
+		*head = new;
+	
+	}
+	return (*head);
 }
