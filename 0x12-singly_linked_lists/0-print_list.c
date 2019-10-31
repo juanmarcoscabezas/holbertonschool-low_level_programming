b26d6d2e9b7fc4db234ca61cacb26f2244569b89
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-31 19:08:53 +0000

    Working on task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,6 +13,6 @@ size_t print_list(const list_t *h)
 	if (!(h->str))
 		printf("[0] (nil)\n");
 	else
-		printf("[%d] %s\n", h->len, h->str);
+		printf("[%i] %s\n", h->len, h->str);
 	return (print_list(h->next) + 1);
 }
