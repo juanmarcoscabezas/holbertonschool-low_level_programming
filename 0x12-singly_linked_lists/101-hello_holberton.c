9e586375a18f4869ea46aaf3db094a1f64052487
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-31 23:30:15 +0000

    Working on advanced task 101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * main - Entry function
+ * Description: This function starts before main
+ * @void: Void param
+ * Return: Always 0;
+ */
+int main(void)
+{
+	printf("Hello, Holberton\n");
+	return (0);
+}
