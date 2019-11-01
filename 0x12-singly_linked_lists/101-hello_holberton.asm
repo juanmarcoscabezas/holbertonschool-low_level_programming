78cd6e2b75409e09ad470e210ba80bef684ba727
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-11-01 02:00:08 +0000

    Hello, holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,13 @@
-	.file	"101-hello_holberton.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB2:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$.LC0, %edi
-	call	puts
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE2:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~14.04~ppa1) 7.4.0"
-	.section	.note.GNU-stack,"",@progbits
+section .data
+
+    message db "Hello, Holberton", 10
+
+section .text
+
+global _start
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, 14
+    syscall
