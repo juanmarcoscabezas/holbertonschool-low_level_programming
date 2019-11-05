ff8da0b09048556f5f418376bf1d0839a4088866
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-11-05 22:34:24 +0000

    fixed?

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,13 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int counter = 0;
 
 	new_node->n = n;
-	new_node->next = NULL;
 
-	if (!head)
-	{
-		free(new_node);
-		return (NULL);
-	}
 	if (!new_node)
 		return (NULL);
 	if (idx == 0)
