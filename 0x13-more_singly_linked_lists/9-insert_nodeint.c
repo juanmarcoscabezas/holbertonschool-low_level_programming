5b3de3483011f9eb232e25464534f06d5a07f921
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-11-05 22:43:44 +0000

    Betty fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	for(counter = 0; counter < idx - 1; counter++)
+	for (counter = 0; counter < idx - 1; counter++)
 	{
 		if (!node_to_find)
 		{
