ee6d7f5c815cb224ff19a01f6b68b0c3949c2be3
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-11-12 23:59:02 +0000

    Removing strlen by _strlen

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
 
 /**
+ * _strlen - Return the len of a string
+ * Description: This function shows the length of a given string
+ * @s: Pointer that contains the string
+ * Return: @s len
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
  * create_file - Creates a file
  * Description: This function creates file
  * @filename: File to create
@@ -19,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	w = write(fd, text_content, strlen(text_content));
+	w = write(fd, text_content, _strlen(text_content));
 
 	if (w == -1)
 		return (-1);
