4fdb25de5a3fb9cbe20e32ab143af202b79f4c39
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-11-12 22:38:45 +0000

    Working on i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - Reads a text file and prints
+ * Description: This function reads a text file and prints it
+ * to the POSIX standart output
+ * @filename: File to print
+ * @letters: Number of letters it should read and print
+ * Return: The number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL, return 0
+ * if write fails or does not write the expect amout of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - putchar function
+ * Description: This function prints characters
+ * @c: is the entry character that has to be printed
+ * Return: void
+ */
+int _putchar(char c);
+
+
+
+
+
+
+
+
+#endif
