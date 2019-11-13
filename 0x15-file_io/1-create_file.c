f7a26837b9d80b4c79285ac618e300ec13d90c74
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-11-13 00:04:27 +0000

    Cleaning code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,12 +32,13 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
 
-	w = write(fd, text_content, _strlen(text_content));
+	if (text_content)
+		w = write(fd, text_content, _strlen(text_content));
 
 	if (w == -1)
 		return (-1);
