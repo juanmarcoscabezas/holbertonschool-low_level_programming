3986c770f2361c0c2a19c57ff1876603a74bfdaf
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-15 19:48:21 +0000

    Working on advanced task 102

---
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+number = 0
+a = 0
+b = 0
+for i in range(1000):
+    for j in range(1000):
+        if str(i * j) == str(i * j)[::-1]:
+            number = i * j
+            a = i
+            b = j
+print("The largest palindrome number is {:d} * {:d} = {:d}".format(a, b, number))
