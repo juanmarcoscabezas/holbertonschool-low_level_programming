db484d90415bd60ce75e72afb43dcf02be4212e1
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-15 15:29:52 +0000

    Case when malloc fails

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,6 +12,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
