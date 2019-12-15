c2cd0ab5a32ee8c76a39784b8e504b3b34a5bdea
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-15 00:11:20 +0000

    Case when the head is not the start

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,6 +11,10 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *head = h;
 	size_t number_nodes = 0;
 
+	while (head->prev)
+	{
+		head = head->prev;
+	}
 	while (head)
 	{
 		printf("%d\n", head->n);
