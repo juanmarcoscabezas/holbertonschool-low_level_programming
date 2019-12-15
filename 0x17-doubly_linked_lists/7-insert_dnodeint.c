953a33c2cec2d9a4822ddcc05ee2ec7d8d193322
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-15 16:27:20 +0000

    Set head to the new_node in case idx 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,6 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		actual_node->prev = new_node;
 		new_node->next = actual_node;
+		*h = new_node;
 		return (new_node);
 	}
 	while (actual_node)
