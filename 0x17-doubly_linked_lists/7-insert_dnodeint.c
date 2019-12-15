a169f353b11f3786cd033033d457584fb7000650
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-15 18:30:30 +0000

    Testing NULL case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,16 +14,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *actual_node;
 	unsigned int len = 0;
 
-	if (h == NULL)
+	if (!h)
 		return (NULL);
-	if (*h == NULL && idx != 0)
+	if (!*h == NULL && idx != 0)
 		return (NULL);
-
-	actual_node = *h;
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
@@ -32,6 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 	new_node->next = NULL;
 
+	actual_node = *h;
 	while (actual_node->next)
 	{
 		if (len == idx)
