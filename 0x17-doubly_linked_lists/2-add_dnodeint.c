2f07fafd2dd5403743275ca273c21fb058c726b6
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-15 15:27:51 +0000

    Now the numbers are printed

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,9 +22,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		{
 			(*head) = (*head)->prev;
 		}
+		(*head)->prev = new_node;
 		new_node->next = (*head);
 		(*head) = new_node;
 		return (*head);
 	}
-	return (new_node);
+	*head = new_node;
+	return (*head);
 }
