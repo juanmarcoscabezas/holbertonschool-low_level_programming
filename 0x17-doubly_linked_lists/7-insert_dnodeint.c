c8efc8f917418795777c0ae7dbe29e58aa52c066
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-15 17:46:05 +0000

    Refactoring code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,28 +11,28 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *actual_node = *h;
+	dlistint_t *actual_node;
 	unsigned int len = 0;
 
-	if (*h == NULL)
+	if (h == NULL)
 		return (NULL);
+	if (*h == NULL && idx != 0)
+		return (NULL);
+
+	actual_node = *h;
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
 
-	if (idx == 0)
-	{
-		actual_node->prev = new_node;
-		new_node->next = actual_node;
-		*h = new_node;
-		return (new_node);
-	}
-	while (actual_node)
+	while (actual_node->next)
 	{
 		if (len == idx)
 		{
@@ -45,6 +45,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		actual_node = actual_node->next;
 		len++;
 	}
+	if (len + 1 == idx)
+		return (add_dnodeint_end(h, n));
 	free(new_node);
 	return (NULL);
 }
