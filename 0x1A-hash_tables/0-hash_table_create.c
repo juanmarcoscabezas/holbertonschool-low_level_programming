02c4209bf074889e4d6a667c3207c9b42744e59f
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-01-18 20:46:02 +0000

    Fixing some assignment

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,10 +10,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
+	if (size == 0)
+		return (NULL);
+
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
-		return NULL;
+		return (NULL);
+
 	table->size = size;
-	table->array = NULL;
-	return table;
+	table->array = (hash_node_t **) malloc(sizeof(hash_node_t) * size);
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
 }
